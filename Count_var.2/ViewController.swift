9abe625b32baf9d97c772b9f37e42ec1aa9a1ade
久保帆奈美 <honami@honami-no-macbook-air.local>
2016-02-01 12:19:00 +0900

    Initial Commit

---
+++ Count_var.2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Count_var.2
+//
+//  Created by Honami on 2016/02/01.
+//  Copyright © 2016年 Honami. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
