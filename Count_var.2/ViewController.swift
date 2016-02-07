7eb51b76d934afb77ade6c28e6ecc9047f288457
Honami <honami.725725@gmail.com>
2016-02-07 15:51:08 +0900

    First commit.

--- Count_var.2/ViewController.swift
+++ Count_var.2/ViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +23,29 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func plus(){
+        number = number + 1
+        label.text = String(number)
+        
+        if number < 0 {
+            label.textColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 1.0)
+        } else {
+            label.textColor = UIColor(red: 0.3, green: 0.2, blue: 0.1, alpha: 1.0)
+        }
 
+    }
 
+    @IBAction func minus(){
+        number = number - 1
+        label.text = String(number)
+    
+        if number < 0 {
+            label.textColor = UIColor(red: 0.6, green: 0.4, blue: 0.2, alpha: 1.0)
+        } else {
+            label.textColor = UIColor(red: 0.3, green: 0.2, blue: 0.1, alpha: 1.0)
+        }
+    }
+    
 }
 
