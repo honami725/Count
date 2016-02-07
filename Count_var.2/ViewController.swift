35583bac8545fb94e4810b62eef0e0ce2c00a7ab
Honami <honami.725725@gmail.com>
2016-02-07 16:27:20 +0900

    改行した

--- Count_var.2/ViewController.swift
+++ Count_var.2/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     var number: Int = 0
     @IBOutlet var label: UILabel!
     
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
